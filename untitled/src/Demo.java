5bfe89a4c6fb422eef22faaf8e502ac305e4fd45
li huanhuan <1182593146@qq.com>
2020-07-05 17:54:59 +0800

    演示提交

---
+++ untitled/src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("nihaojdhsdf");
+        System.out.println("nihaojdhsdf1");
+        System.out.println("nihaojdhsdf2");
+        System.out.println("nihaojdhsdf3");
+        System.out.println("nihaojdhsdf3");
+        System.out.println("nihaojdhsdf5");
+
+    }
+}
